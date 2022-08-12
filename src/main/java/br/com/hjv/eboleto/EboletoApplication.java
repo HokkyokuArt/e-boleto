a022dd427d30cbbefcd255c3611d0d2ce73e39a0
Jean Artico <jeanartico13@hotmail.com>
2022-08-12 10:30:40 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/br/com/hjv/eboleto/EboletoApplication.java
@@ -0,0 +1,13 @@
+package br.com.hjv.eboleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EboletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EboletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hjv/eboleto/EboletoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hjv.eboleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EboletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
