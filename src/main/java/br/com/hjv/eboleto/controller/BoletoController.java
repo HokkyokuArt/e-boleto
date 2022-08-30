d26382347d74fa905a6373af0f3270cd9380b0e6
Jean Artico <jeanartico13@hotmail.com>
2022-08-30 10:36:13 -0300

    EndPoint cliente/{id}/meus-boletos

--- src/main/java/br/com/hjv/eboleto/controller/BoletoController.java
+++ src/main/java/br/com/hjv/eboleto/controller/BoletoController.java
@@ -2,15 +2,7 @@ package br.com.hjv.eboleto.controller;
 
 import br.com.hjv.eboleto.core.CrudController;
 import br.com.hjv.eboleto.domain.Boleto;
-import br.com.hjv.eboleto.domain.Cliente;
 import br.com.hjv.eboleto.dto.BoletoDTO;
-import br.com.hjv.eboleto.repository.BoletoRepository;
-
-import java.util.List;
-
-import javax.websocket.server.PathParam;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,16 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/boleto")
 public class BoletoController extends CrudController<Boleto, BoletoDTO, Long> {
 
-    public BoletoRepository getRepository(){
-        return (BoletoRepository) this.repository;
-    }
-
-    @GetMapping("/cliente={id}")
-    public List<Boleto> consultarBoletosPorId(@PathParam("id") Cliente id){
-        
-        var boletos = this.getRepository().constultaBoletosPorCliente(id);
-
-        return boletos;
-
-    }
+    
 }
--- src/main/java/br/com/hjv/eboleto/controller/ClienteController.java
+++ src/main/java/br/com/hjv/eboleto/controller/ClienteController.java
@@ -1,12 +1,35 @@
 package br.com.hjv.eboleto.controller;
 
 import br.com.hjv.eboleto.core.CrudController;
+import br.com.hjv.eboleto.domain.Boleto;
 import br.com.hjv.eboleto.domain.Cliente;
 import br.com.hjv.eboleto.dto.ClienteDTO;
+import br.com.hjv.eboleto.repository.BoletoRepository;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController extends CrudController<Cliente, ClienteDTO, Long> {
+    
+    @Autowired
+    protected BoletoRepository boletoRepository;
+    
+    public BoletoRepository getRepository(){
+        return (BoletoRepository) this.boletoRepository;
+    }
 
+    @GetMapping("/{id}/meus-boletos")
+    public List<Boleto> consultarBoletosPorId(@PathParam("id") Cliente id){
+        
+        var boletos = this.getRepository().constultaBoletosPorCliente(id);
+
+        return boletos;
+
+    }
 }
